513005479447e107afa94d487f79a76f6302f4e4
BruceSpruce <bricerandy2001@gmail.com>
2023-09-02 18:50:38 -0500

    add init_world def and fix loop

--- assignment01/worldgen.c
+++ assignment01/worldgen.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #define GRID_HEIGHT 80
 #define GRID_WIDTH 24
 
+static int8_t gridMatrix[GRID_HEIGHT][GRID_WIDTH];
+
+void init_world() 
+{
+	// TODO: impliment initalization of tiles
+}
 
 int main(int argc, char* argv[])
 {
-    int gridMatrix[GRID_HEIGHT][GRID_WIDTH];
-    
-    for(int i =0; i < GRID_HEIGHT; i++){
-	gridMatrix[0][i] = 37;
-	for(int j =0; j < GRID_WIDTH -3; j++){
-	    if(gridMatrix[i][j] != 64) continue;
-	    gridMatrix[i][j] = 64;
-	    printf("%c", gridMatrix[i][j]);
-	}
+
+	init_world();
+
+    for(int i=0; i < GRID_HEIGHT - 3; i++) {
+		for(int j=0; j < GRID_WIDTH; j++) {
+			gridMatrix[i][j] = 64;
+			printf("%c", gridMatrix[i][j]);
+		}
+		printf("\n");
     }
 }
