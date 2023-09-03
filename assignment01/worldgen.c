b9c3458e4b88043374990d72a4d83f421381d8b0
BruceSpruce <bricerandy2001@gmail.com>
2023-09-03 12:38:25 -0500

    init world and remove buffer file

--- assignment01/worldgen.c
+++ assignment01/worldgen.c
@@ -1,25 +1,53 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "tiles.h"
 
-#define GRID_HEIGHT 24
+#define GRID_HEIGHT 24 - 3
 #define GRID_WIDTH 80
 
 static char gridMatrix[GRID_HEIGHT][GRID_WIDTH];
 
 void init_world() 
 {
-	// TODO: impliment initalization of tiles
+	
+	for(int i=0; i < GRID_HEIGHT; i++) {
+		gridMatrix[i][0] = mountainTile.ascii;
+		gridMatrix[i][GRID_WIDTH-1] = mountainTile.ascii;
+	}
+
+	for(int j=0; j < GRID_WIDTH; j++) {
+		gridMatrix[0][j] = mountainTile.ascii;
+		gridMatrix[GRID_HEIGHT-1][j] = mountainTile.ascii;
+	}
+
+	for(int i=1; i < GRID_HEIGHT - 1; i++) {
+		for(int j=1; j < GRID_WIDTH - 1; j++) {
+			gridMatrix[i][j] = clearing.ascii;
+		}
+	}
+
+}
+
+void printGrid()
+{
+	printf("\x1b[H");
+	for(int i=0; i < GRID_HEIGHT; i++) {
+		for(int j=0; j < GRID_WIDTH; j++) {
+			putchar(gridMatrix[i][j]);
+		}
+		putchar('\n');
+    }
 }
 
 int main(int argc, char* argv[])
 {
 	init_world();
-
-    for(int i=0; i < GRID_HEIGHT - 3; i++) {
-		for(int j=0; j < GRID_WIDTH; j++) {
-			gridMatrix[i][j] = 64;
-			printf("%c", gridMatrix[i][j]);
-		}
-		printf("\n");
-    }
+	printf("\x1b[2J");
+	for(;;) {
+		// printf("\033[?25l");  // hide cursor
+		printGrid();
+		// printf("\033[?25h"); // show cursor
+		usleep(100000);  // 100ms
+	}
+    return 0;
 }
